3ad792b2272a850154a99f07614de4a0a6c5f451
AdSoyad <email@example.com>
2025-12-02 17:28:48 +0400

    first task

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
-const getProductData = async () => {
+const container = document.getElementById('container')
+
+const getAllProduct = async () => {
   try {
     const res = await fetch("https://ilkinibadov.com/api/v1/products")
-   
     const data = await res.json()
-    console.log(data);
-
+    renderItems(data.products)
   } catch (error) {
     console.error(error);
   }
 }
-getProductData()
+getAllProduct()
 
-const getLogin = async () => {
-  try {
-    const res = await fetch("https://ilkinibadov.com/api/v1/auth/login", {
-      method: "POST",
-        body: JSON.stringify({
-        email: "ilkin@example.com",
-        password: "12345678"
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
+const renderItems = (products) => {
+  products.forEach(product => {
+    const div = document.createElement('div')
+    const img = document.createElement('img')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    const span = document.createElement('span')
 
-    const data1 = await res.json()
-    console.log(data1);
+    img.src = product.images[0]
+    img.classList.add('size-[200px]', 'object-contain', 'ml-15')
 
-  } catch (error) {
-    console.error(error);
-  }
+    h3.innerText = product.title
+    h3.classList.add("font-bold", 'text-xl')
+
+    p.innerText = product.description
+    p.classList.add('text-x')
+
+    span.innerText = `${product.currency} ${product.price}`
+    span.classList.add('text-red-500', 'font-bold')
+
+    div.classList.add('border', 'border-zinc-300', 'p-3', 'rounded-xl', 'flex', 'flex-col', 'justify-center', 'gap-4', 'shadow-xl', 'cursor-pointer')
+
+    div.append(img)
+    div.append(h3)
+    div.append(p)
+    div.append(span)
+    container.append(div)
+  });
 }
-getLogin()
-
-const getFindProductData = async () => {
-  try {
-    const res = await fetch("https://ilkinibadov.com/api/v1/search?searchterm=lamp")
-   
-    const data2 = await res.json()
-    console.log(data2);
-
-  } catch (error) {
-    console.error(error);
-  }
-}
-getFindProductData()
