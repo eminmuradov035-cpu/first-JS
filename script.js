4e67b3b248d0609361eb0f74c5ae7c46fa53da9e
AdSoyad <email@example.com>
2025-12-08 21:34:02 +0400

    hello

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-const container = document.getElementById('container')
+class BankAccount{
+  #balance
+  constructor(){
+    this.#balance = 0
+  }
 
-const getAllProduct = async () => {
-  try {
-    const res = await fetch("https://ilkinibadov.com/api/v1/products")
-    const data = await res.json()
-    renderItems(data.products)
-  } catch (error) {
-    console.error(error);
+  deposit (ammount) {
+    if (ammount > 0) {
+      this.#balance += ammount
+    } 
+  }
+
+  withdraw (ammount) {
+    if (ammount > 0 && ammount <= this.#balance) {
+      this.#balance -= ammount
+    }
+  }
+
+  tellBallance() {
+    return this.#balance
   }
 }
-getAllProduct()
 
-const renderItems = (products) => {
-  products.forEach(product => {
-    const div = document.createElement('div')
-    const img = document.createElement('img')
-    const h3 = document.createElement('h3')
-    const p = document.createElement('p')
-    const span = document.createElement('span')
-
-    img.src = product.images[0]
-    img.classList.add('size-[200px]', 'object-contain', 'ml-15')
-
-    h3.innerText = product.title
-    h3.classList.add("font-bold", 'text-xl')
-
-    p.innerText = product.description
-    p.classList.add('text-x')
-
-    span.innerText = `${product.currency} ${product.price}`
-    span.classList.add('text-red-500', 'font-bold')
-
-    div.classList.add('border', 'border-zinc-300', 'p-3', 'rounded-xl', 'flex', 'flex-col', 'justify-center', 'gap-4', 'shadow-xl', 'cursor-pointer')
-
-    div.append(img)
-    div.append(h3)
-    div.append(p)
-    div.append(span)
-    container.append(div)
-  });
-}
+const myAccount = new BankAccount
+myAccount.deposit(900)
+myAccount.deposit(1300)
+console.log(myAccount.tellBallance());
